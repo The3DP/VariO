892736a762b5cf19cdae5c4dc79563740ad80045
DareN <daren@markandtraci.com>
2025-03-28 18:59:46 -0500

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,19 +1,28 @@
-  # This is section 1 of the program.
-def exit():
- ex = 2
- while ex != 1 and ex != 0:
-  ex = int(input("enter 1 to continue or 0 to exit"))
- return ex
+"""
+Module docstring
+"""
 
-#variable to keep track of loop iterations
-counter = 0
+import sys
 
-#create variable for bool
-stay = True
+def exit_program():
+    """
+    This function prompts the user to enter 1 to continue or 0 to exit.
+    """
+    ex = 2
+    while ex not in {1, 0}:  # using 'in' for comparison
+        ex = int(input("Enter 1 to continue or 0 to exit: "))
+    return ex
 
-while stay:
- print(counter)
- counter+=1
- stay = exit()
+# Variable to keep track of loop iterations
+COUNTER = 0
+
+# Create variable for bool
+STAY = True
+
+while STAY:
+    print(COUNTER)
+    COUNTER += 1
+    STAY = exit_program()
 
 print("You have exited the loop")
+
