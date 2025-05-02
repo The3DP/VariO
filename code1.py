c9cab598c0a278a54c6731b9c14aa10068558dfa
DareN <daren@markandtraci.com>
2025-05-02 18:18:34 -0500

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,24 +1,10 @@
-def handle_user_choice(ex):
-    if ex == 1:
-        print("\nYou have chosen to exit the program. Goodbye!")
-    elif ex == 0:
-        print("\nYou have chosen to continue. The program will continue running.")
-    else:
-        print("Error: Input out of expected range. Please enter only 1 or 0.")
+from utils import handle_user_choice, get_user_input
 
 def main():
-    ex = 2
     print("Welcome to the program!")
     print("You can enter '1' to exit the program or '0' to continue the program.")
-
-    while ex not in (0, 1):
-        user_input = input("\nPlease enter your choice (1 to exit, 0 to continue): ")
-
-        if user_input.isdigit():
-            ex = int(user_input)
-            handle_user_choice(ex)
-        else:
-            print("Error: Invalid input. Please enter a valid number (1 to exit, 0 to continue).")
+    ex = get_user_input()
+    handle_user_choice(ex)
 
 if __name__ == "__main__":
     main()
